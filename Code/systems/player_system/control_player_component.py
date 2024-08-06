f46320ffc0b6a81a5c901bf8e829aff043544e81
themanyfaceddemon <neko-d-05@mail.ru>
2024-08-06 20:36:15 +0300

    Дописал компонент для работы с плеер компонентом

--- Code/systems/player_system/control_player_component.py
+++ Code/systems/player_system/control_player_component.py
@@ -1,8 +1,34 @@
+from typing import Any, Dict, Type
+
 from systems.entity_system import BaseComponent
+
 
 class ControlPlayerComponent(BaseComponent):
     __slots__ = []
     
-    def __init__(self) -> None:
-        super().__init__('ControlPlayerComponent')
+    def __init__(self, login: str) -> None:
+        super().__init__('ControlPlayerComponent') 
+        self.login: str = login
+    
+    def __repr__(self) -> str:
+        """Возвращает строковое представление компонента.
+        Этот метод должен быть переопределен в подклассах, чтобы возвращать строку,
+        содержащую имя класса и параметры его конструктора.
 
+        Returns:
+            str: Строковое представление компонента.
+        """
+        return f"ControlPlayerComponent(login={self.login})"
+
+    @staticmethod
+    def get_type_hints() -> Dict[str, Type[Any]]:
+        """Возвращает словарь с именами переменных и их типами.
+        Этот метод должен быть переопределен в подклассах, чтобы возвращать корректные типы
+        для каждого атрибута компонента.
+
+        Returns:
+            Dict[str, Type[Any]]: Словарь с именами переменных и их типами.
+        """
+        return {
+            'login': str
+        }
--- Code/systems/player_system/player_manager.py
+++ Code/systems/player_system/player_manager.py
@@ -2,6 +2,8 @@ from typing import Dict, List
 
 from systems.entity_system import BaseEntity
 from systems.misc import GlobalClass
+from systems.player_system.control_player_component import \
+    ControlPlayerComponent
 
 
 class PlayerManager(GlobalClass):
@@ -12,20 +14,36 @@ class PlayerManager(GlobalClass):
             self._initialized = True
             self._player_dict: Dict[str, List[BaseEntity]] = {}
 
-    def add_player_entity(self, player: str, obj: BaseEntity) -> None:
-        if player not in self._player_dict:
-            self._player_dict[player] = []
+    def register_entitys(self, entitys: List[BaseEntity]):
+        entitys = list(entitys)
         
-        self._player_dict[player].append(obj) # TODO: Компонент для указания что мы контролируем данный объект
+        for entity in entitys:
+            comp = entity.get_component('ControlPlayerComponent')
+            if comp:
+                login = comp.login
+                if login not in self._player_dict:
+                    self._player_dict[login] = []
+                
+                self._player_dict[login].append(entity)
 
-    def remove_player_entity(self, player: str, uid: int) -> None:
-        if player in self._player_dict:
+    def add_player_entity(self, login: str, entity: BaseEntity) -> None:
+        if login not in self._player_dict:
+            self._player_dict[login] = []
+        
+        self._player_dict[login].append(entity)
+        entity.add_component(ControlPlayerComponent(login))
+
+    def remove_player_entity(self, login: str, uid: int) -> None:
+        if login in self._player_dict:
             new_player_entities = []
-            for entity in self._player_dict[player]:
+            for entity in self._player_dict[login]:
                 if entity.uid != uid:
                     new_player_entities.append(entity)
                 
                 else:
-                    pass  # TODO: Компонент для указания что мы контролируем данный объект
+                    entity.remove_component("ControlPlayerComponent")
             
-            self._player_dict[player] = new_player_entities
+            self._player_dict[login] = new_player_entities
+
+    def get_player_entitys(self, login: str) -> List[BaseEntity]:
+        pass
